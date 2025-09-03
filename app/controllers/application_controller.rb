67c1bee803b33abbd257ad98d8e6e0eff0e7dff1
sukhdev singh <sukhdevsingh00133@gmail.com>
2025-09-03 18:13:47 +0530

    toy_app create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   #allow_browser versions: :modern
-  def hello 
-    render html: "hello sukhdev"
-  end
+  # def hello 
+  #   render html: "hello sukhdev"
+  # end
 
-  def goodbye
-    render html: "goodbye"
-  end
+  # def goodbye
+  #   render html: "goodbye"
+  # end
 end
